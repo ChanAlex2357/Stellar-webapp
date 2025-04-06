0dcea4f36512925985a26aac4372116f5c3fc792
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-04-06 11:20:10 +0300

    nav list

---
+++ src/screens/NavListProduit.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Accueil from '@/screens/ListProduitPanier.vue'
+
+</script>
+<template>
+    <header id="header">
+      <div class="container">
+        <div id="logo-container">
+          <img src="@/assets/image/logo.png" alt="Logo" id="logo">
+        </div>
+          <nav id="menu">
+            <a href="">Accueil</a>
+            <a href="">Caractéristiques</a>
+            <a href="">Gallery</a>
+            <a href="">Contact</a>
+            <a href="">Produits</a>
+        </nav>
+      </div>
+    </header>
+        <Accueil></Accueil>
+      <div id="orange-bar"></div>
+      <Caracteristique></Caracteristique>
+      <Gallerie></Gallerie>
+      <Contact></Contact>
+      <NosProduit></NosProduit>
+    <Footer></Footer>
+</template>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+#orange-bar {
+  background-color: var(--secondary-color);
+  height: 50px;
+}
+</style>
