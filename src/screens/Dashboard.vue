1913c2b5d63daf94030b550cb4730efd007f32ea
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-04-07 21:31:20 +0300

    dashboard loading

--- src/screens/Dashboard.vue
+++ src/screens/Dashboard.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted , ref} from 'vue';
 import NavListProduit from './NavListProduit.vue';
+
+const loading = ref(true);
 
 onMounted(() => {
   if(localStorage.getItem('DOLAPIKEY') === null){
@@ -13,6 +15,13 @@ onMounted(() => {
     <NavListProduit></NavListProduit>
     <div class="container">
         <h1>Dashboard</h1>
+        <div v-if="loading" class="loading">
+          <BicycleSpinner />
+        </div>
+        <div v-else class="dashboard-content">
+            <h2>Welcome to the Dashboard</h2>
+            <p>Your dashboard content goes here.</p>
+        </div>
     </div>
 </template>
 
